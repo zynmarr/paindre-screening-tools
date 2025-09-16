8fd7a0c394369c43acfbe58da39081a0647a259d
zynmarr <muammark0024@gmail.com>
2025-09-16 23:51:48 +0700

    update swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,7 @@
 import Flutter
 import UIKit
-import flutter_local_notifications
 import Firebase
-import FirebaseCore
+import flutter_local_notifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,23 +9,22 @@ import FirebaseCore
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    FirebaseApp.configure()
-
-
-
-    // let providerFactory = AppCheckDebugProviderFactory()
-    // AppCheck.setAppCheckProviderFactory(providerFactory)
     
+    // 1. Daftarkan semua plugin Flutter terlebih dahulu. Ini sangat penting.
+    GeneratedPluginRegistrant.register(with: self)
+    
+    // 2. Setelah plugin terdaftar, baru konfigurasikan Firebase.
+    FirebaseApp.configure()
+    
+    // Konfigurasi untuk notifikasi
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
 
     FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
         GeneratedPluginRegistrant.register(with: registry)
     }
 
-    if #available(iOS 10.0, *) {
-      UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
-    }
-
-    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
